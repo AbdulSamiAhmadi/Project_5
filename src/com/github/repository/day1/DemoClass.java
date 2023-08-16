2e6d7d8bdec8edbacd51b9fa716867d27241ae20
AbdulSamiAhmadi <140773647+AbdulSamiAhmadi@users.noreply.github.com>
2023-08-16 19:32:39 -0400

    Update DemoClass.java

--- src/com/github/repository/day1/DemoClass.java
+++ src/com/github/repository/day1/DemoClass.java
@@ -2,4 +2,9 @@ package com.github.repository.day1;
 
 public class DemoClass {
 
-}
+  	public static void main(String[] args) {
+
+  System.out.println("Helena");
+	System.out.println("Arista");
+
+}}
